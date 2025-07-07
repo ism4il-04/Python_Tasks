5825ae48a029cc3fa49ea0527888100084a1e86b
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-07-07 14:23:47 +0100

    premier horizontal layout

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("To-do list")
         self.setFixedSize(QSize(800, 500))
         self.setWindowIcon(QIcon("icon.ico"))
+        self.nom_tache = QLineEdit()
+        self.nom_resp = QLineEdit()
+        self.date_tache = QLineEdit()
+        self.button_enregistrer = QPushButton("Enregistrer")
+
+        self.initUI()
+        
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        vbox = QVBoxLayout()
+        hbox = QHBoxLayout() #où on tape le nom de tache ...
+
+        hbox.addWidget(self.nom_tache)
+        hbox.addWidget(self.nom_resp)
+        hbox.addWidget(self.date_tache)
+        hbox.addWidget(self.button_enregistrer)
+        vbox.addLayout(hbox)
+
+        central_widget.setLayout(vbox)
+
         
 
 def main():
